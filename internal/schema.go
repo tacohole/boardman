b986c5537ad81abeb3352a69c38d4f9dff0e8bd0
Troy Coll <troy.coll@gmail.com>
2022-01-03 08:03:29 -0500

    adding single game stats

--- internal/schema.go
+++ internal/schema.go
@@ -30,25 +30,3 @@ type TeamYear struct {
 // sum wpct
 // get conf rank
 // build roster
-
-type PlayerYear struct {
-	Player  Player
-	Season  Season
-	Minutes string  `json:"avg_min" db:"avg_min"`
-	FGM     float32 `json:"fgm" db:"fgm"`
-	FGA     float32 `json:"fga" db:"fga"`
-	FG3M    float32 `json:"fg3m" db:"fg3m"`
-	FG3A    float32 `json:"fg3a" db:"fg3a"`
-	OREB    float32 `json:"oreb" db:"oreb"`
-	DREB    float32 `json:"dreb" db:"dreb"`
-	REB     float32 `json:"reb" db:"reb"`
-	AST     float32 `json:"ast" db:"ast"`
-	STL     float32 `json:"stl" db:"stl"`
-	BLK     float32 `json:"blk" db:"blk"`
-	TO      float32 `json:"to" db:"to"`
-	PF      float32 `json:"pf" db:"pf"`
-	PTS     float32 `json:"pts" db:"pts"`
-	FG_PCT  float32 `json:"fg_pct" db:"fg_pct"`
-	FG3_PCT float32 `json:"fg3_pct" db:"fg3_pct"`
-	FT_PCT  float32 `json:"ft_pct" db:"ft_pct"`
-}
---
+++ internal/seasonAverages.go
@@ -0,0 +1,29 @@
+package internal
+
+import "github.com/google/uuid"
+
+type PlayerYear struct {
+	PlayerID    uuid.UUID
+	BDL_ID      int     `json:"player_id" db:"balldontlie_id"`
+	Season      int     `json:"season" db:"league_year"`
+	GamesPlayed int     `json:"games_played" db:"games_played"`
+	Minutes     string  `json:"avg_min" db:"avg_min"`
+	FGM         float32 `json:"fgm" db:"fgm"`
+	FGA         float32 `json:"fga" db:"fga"`
+	FG3M        float32 `json:"fg3m" db:"fg3m"`
+	FG3A        float32 `json:"fg3a" db:"fg3a"`
+	OREB        float32 `json:"oreb" db:"oreb"`
+	DREB        float32 `json:"dreb" db:"dreb"`
+	REB         float32 `json:"reb" db:"reb"`
+	AST         float32 `json:"ast" db:"ast"`
+	STL         float32 `json:"stl" db:"stl"`
+	BLK         float32 `json:"blk" db:"blk"`
+	TO          float32 `json:"to" db:"to"`
+	PF          float32 `json:"pf" db:"pf"`
+	PTS         float32 `json:"pts" db:"pts"`
+	FG_PCT      float32 `json:"fg_pct" db:"fg_pct"`
+	FG3_PCT     float32 `json:"fg3_pct" db:"fg3_pct"`
+	FT_PCT      float32 `json:"ft_pct" db:"ft_pct"`
+}
+
+// lookup our UUID off BDL_ID
