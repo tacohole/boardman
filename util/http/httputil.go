acb221a4c580a7b4832865f145a48f9ddced713a
Troy Coll <troy.coll@gmail.com>
2022-02-04 07:42:38 -0500

    adding logging back to http

--- util/http/httputil.go
+++ util/http/httputil.go
@@ -22,6 +22,7 @@ func MakeHttpRequest(method string, url string) (*http.Response, error) {
 
 	request.Header.Set("Content-Type", "application/json")
 
+	fmt.Printf("sending request to %s", url)
 	response, err := client.Do(request)
 	if err != nil || response.StatusCode >= 400 {
 		return nil, fmt.Errorf("%s request to %s failed: %d", method, url, response.StatusCode)
